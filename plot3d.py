a403567745962faf14d1ea063ab201dd0f48fb61
kacper1001 <kacperkarcz1001@gmail.com>
2018-12-26 19:31:32 +0100

    Add files via upload

--- plot3d.py
+++ plot3d.py
@@ -6,27 +6,40 @@ from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
 
-
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-# Make data.
-X = np.arange(-5, 5, 0.25)
-Y = np.arange(-5, 5, 0.25)
-X, Y = np.meshgrid(X, Y)
-R = np.sqrt(X**2 + Y**2)
-Z = np.sin(R)
+k = (np.pi * 2 / 0.532)
+theta = 4 * np.pi / 180
+sinteta = np.sin(theta)
+costeta = np.cos(theta)
 
-# Plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+x = np.arange(-7.5, 7.5, 0.1)
+y = x
+X, Y = np.meshgrid(x, y)
 
-# Customize the z axis.
-ax.set_zlim(-1.01, 1.01)
+Is = 4 - 2 * np.cos(2 * k * sinteta * X) - 2 * np.cos(2 * k * sinteta * Y)
+
+Ips = 4 - 2 * np.cos(2 * k * sinteta * X)\
+     - 4 * costeta * np.cos(k * sinteta * (X + Y)) \
+     + 4 * costeta * np.cos(k * sinteta * (X - Y)) \
+     + 2 * np.cos(2 * k * sinteta * Y) * (sinteta *sinteta - costeta*costeta)
+
+Ip = 4 \
+     + 2 * np.cos(2 * k * sinteta * X) * (sinteta * sinteta - costeta*costeta)\
+     - 4 * costeta * np.cos(k * sinteta * (X + Y)) \
+     - 4 * costeta * np.cos(k * sinteta * (-X + Y)) \
+     + 4 * costeta * np.cos(k * sinteta * (X - Y)) \
+     + 4 * costeta * np.cos(k * sinteta * (-X - Y)) \
+     + 2 * np.cos(2 * k * sinteta * Y) * (sinteta * sinteta - costeta*costeta)
+
+surfs = ax.plot_surface(X, Y, Ip, cmap=cm.hot, linewidth=0, antialiased=False)
+
+ax.set_zlim(0, 13)
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+fig.colorbar(surfs, shrink=0.5, aspect=5)
+
 
 plt.show()
